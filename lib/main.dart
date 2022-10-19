6e5fdc7f68decb26c89c6dbefadf5fcec8b967c3
Leonardo Amato <leo.amato@hotmail.com>
2022-10-19 13:07:11 +0200

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Shop App',
+      title: 'My Shop App in dev',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
